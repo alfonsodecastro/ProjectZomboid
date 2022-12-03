4efb7a8f2900986c551c34b94733a1963e42c247
alfonsodecastro <alfonso.decastro@gmail.com>
2022-12-03 02:28:08 +0100

    ahora con boost de ropa v0.5

--- RestrictedXP/media/lua/client/restrictedXP.lua
+++ RestrictedXP/media/lua/client/restrictedXP.lua
@@ -1,13 +1,10 @@
 RestrictedXP = RestrictedXP or {}
-RestrictedXP.showValue = true
 
 nuevoRestricted=true;
+nuevoBoost=true;
 
 local function notValid(prof, skill)
 	local notvalid = false
-	
-	alf=Perks.AMReloading:getName()
-	print("alf" ..  alf)
 	
 	if prof == "unemployed" then
 		if skill== Perks.Blacksmith then
@@ -49,6 +46,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "fireofficer" then
 		if skill== Perks.Blacksmith then
@@ -90,7 +88,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
-		
+		return notvalid
 	end
 	if prof == "policeofficer" then
 		if skill== Perks.Blacksmith then
@@ -129,6 +127,7 @@ local function notValid(prof, skill)
 		if skill== Perks.Woodwork then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "parkranger" then
 		if skill== Perks.Blacksmith then
@@ -161,6 +160,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "constructionworker" then
 		if skill== Perks.Blacksmith then
@@ -205,6 +205,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "securityguard" then
 		if skill== Perks.Blacksmith then
@@ -249,7 +250,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
-		
+		return notvalid
 	end
 	if prof == "carpenter" then
 		if skill== Perks.Blacksmith then
@@ -291,7 +292,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
-		
+		return notvalid
 	end
 	if prof == "burglar" then
 		if skill== Perks.Blacksmith then
@@ -333,6 +334,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "chef" then
 		if skill== Perks.Blacksmith then
@@ -374,6 +376,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "repairman" then
 		if skill== Perks.Blacksmith then
@@ -415,6 +418,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "farmer" then
 		if skill== Perks.Blacksmith then
@@ -453,6 +457,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "fisherman" then
 		if skill== Perks.Blacksmith then
@@ -494,7 +499,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
-		
+		return notvalid
 	end
 	if prof == "doctor" then
 		if skill== Perks.Blacksmith then
@@ -536,6 +541,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "veteran" then
 		if skill== Perks.Blacksmith then
@@ -571,6 +577,7 @@ local function notValid(prof, skill)
 		if skill== Perks.Woodwork then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "nurse" then
 		if skill== Perks.Blacksmith then
@@ -612,6 +619,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "lumberjack" then
 		if skill== Perks.Blacksmith then
@@ -650,7 +658,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
-		
+		return notvalid
 	end
 	if prof == "fitnessInstructor" then
 		if skill== Perks.Blacksmith then
@@ -695,6 +703,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "burgerflipper" then
 		if skill== Perks.Blacksmith then
@@ -736,6 +745,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "electrician" then
 		if skill== Perks.Blacksmith then
@@ -777,7 +787,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
-		
+		return notvalid
 	end
 	if prof == "engineer" then
 		if skill== Perks.Blacksmith then
@@ -816,6 +826,7 @@ local function notValid(prof, skill)
 		if skill== Perks.Woodwork then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "metalworker" then
 		if skill== Perks.Blacksmith then
@@ -854,6 +865,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
+		return notvalid
 	end
 	if prof == "mechanics" then
 		if skill== Perks.Blacksmith then
@@ -892,7 +904,7 @@ local function notValid(prof, skill)
 		if skill== Perks.AMReloading then
 			notvalid=true;
 		end
-		
+		return notvalid
 	end
 	if prof == "ammospecialist" then
 		if skill== Perks.Blacksmith then
@@ -934,17 +946,215 @@ local function notValid(prof, skill)
 		if skill== Perks.Woodwork then
 			notvalid=true;
 		end
+		return notvalid
 	end
-	return notvalid
+end
+
+local function getBoost(prof, inv)
+	local _boost=0
+	
+	if prof == "unemployed" then
+		return _boost
+	end
+	if prof == "fireofficer" then
+		if inv.contains("Trousers_Fireman") then
+			_boost=_boost + 5
+		end
+		if inv.contains("Hat_Fireman") then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "policeofficer" then
+		if (inv:contains("Trousers_PoliceGrey") 
+			or inv:contains("Trousers_Police")
+		) then
+			_boost=_boost + 5
+		end
+		if (inv:contains("Shirt_PoliceGrey") 
+			or inv:contains("Shirt_PoliceBlue")
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "parkranger" then
+		if (inv:contains("Trousers_Ranger")) then
+			_boost=_boost + 5
+		end
+		if (inv:contains("Jacket_Ranger")) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "constructionworker" then
+		if (inv:contains("Hat_HardHat")) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "securityguard" then
+		return _boost	
+	end
+	if prof == "carpenter" then
+		return _boost
+	end
+	if prof == "burglar" then
+		if (inv:contains("Hat_BalaclavaFull") 
+			or inv:contains("Hat_BalaclavaFace") 
+			or inv:contains("Hat_BalaclavaSpyBlue") 
+			or inv:contains("Hat_BalaclavaSpyRed")
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "chef" then
+		if (inv:contains("Thino1_Apron") 
+			or inv:contains("Apron_Black") 
+			or inv:contains("Apron_White") 
+			or inv:contains("Apron_LeGourmet")
+			or inv:contains("Apron_WhiteTEXTURE")
+			or inv:contains("Apron_Yellow")
+			or inv:contains("Apron_DonutAZ")
+			or inv:contains("Apron_Short")
+			or inv:contains("Apron_Jay")
+			or inv:contains("Apron_PileOCrepe")
+			or inv:contains("Apron_PizzaWhirled")
+			or inv:contains("Apron_Spiffos")
+			or inv:contains("Apron_GreenSeaHorse")
+			or inv:contains("Apron_:iceCream")
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "repairman" then
+		return _boost
+	end
+	if prof == "farmer" then
+		if (inv:contains("Dungarees") 
+			or inv:contains("DungareesWhite")
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "fisherman" then
+		if (inv:contains("Hat_BucketHat")) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "doctor" then
+		if (inv:contains("Jacket_Doctor2") 
+			or inv:contains("Jacket_Doctor3") 
+			or inv:contains("JacketLong_Doctor") 
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "veteran" then
+		if (inv:contains("Hat_Beret") 
+			or inv:contains("Hat_BeretArmy") 
+			or inv:contains("Hat_BeretBlack") 
+			or inv:contains("Hat_BeretMime") 
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "nurse" then
+		if (inv:contains("Jacket_Doctor2") 
+			or inv:contains("Jacket_Doctor3") 
+			or inv:contains("JacketLong_Doctor") 
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "lumberjack" then
+		if (inv:contains("Shirt_LumberjackShort") 
+			or inv:contains("Shirt_LumberjackTheyLive") 
+			or inv:contains("Shirt_LumberjackShortRed") 
+			or inv:contains("Shirt_Lumberjack") 
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "fitnessInstructor" then
+		if (inv:contains("Hat_SweatbandOGP") 
+			or inv:contains("Hat_SweatbandRBW") 
+			or inv:contains("Hat_SweatbandBlue") 
+			or inv:contains("Hat_SweatbandPurple")
+			or inv:contains("Hat_SweatbandPink")
+		) then
+			_boost=_boost + 5
+		end
+		if (inv:contains("Shorts_ShortFormal") 
+			or inv:contains("Shorts_ShortSport")
+			or inv:contains("Shorts_ShortDenim")
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "burgerflipper" then
+		if (inv:contains("Thino1_Apron") 
+			or inv:contains("Apron_Black") 
+			or inv:contains("Apron_White") 
+			or inv:contains("Apron_LeGourmet")
+			or inv:contains("Apron_WhiteTEXTURE")
+			or inv:contains("Apron_Yellow")
+			or inv:contains("Apron_DonutAZ")
+			or inv:contains("Apron_Short")
+			or inv:contains("Apron_Jay")
+			or inv:contains("Apron_PileOCrepe")
+			or inv:contains("Apron_PizzaWhirled")
+			or inv:contains("Apron_Spiffos")
+			or inv:contains("Apron_GreenSeaHorse")
+			or inv:contains("Apron_:iceCream")
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "electrician" then
+		return _boost
+	end
+	if prof == "engineer" then
+		return _boost
+	end
+	if prof == "metalworker" then
+		return _boost
+	end
+	if prof == "mechanics" then
+		if (inv:contains("Boilersuit_BigDaddy") 
+			or inv:contains("Boilersuit_GhostbustersVenkman") 
+			or inv:contains("Boilersuit_GhostbustersSpengler") 
+			or inv:contains("Boilersuit_GhostbustersStantz")
+			or inv:contains("Boilersuit_GhostbustersZeddemore")
+			or inv:contains("Boilersuit_Halloween")
+			or inv:contains("Boilersuit_Yellow")
+			or inv:contains("Boilersuit")
+			or inv:contains("Boilersuit_BlueRed")
+			or inv:contains("Red_LongJohns")
+		) then
+			_boost=_boost + 5
+		end
+		return _boost
+	end
+	if prof == "ammospecialist" then
+		return _boost
+	end
 end
 
 local function restrictXP(player, skill, level)
 	local prof=player:getDescriptor():getProfession();
-	print("Profesion: " .. prof)
 	
 	if skill ~= nil then
-		local perkName = skill:getName()
-		
 		if not player:isNPC() then
 			if notValid(prof,skill) then
 				if nuevoRestricted == true then
@@ -953,9 +1163,19 @@ local function restrictXP(player, skill, level)
 				else
 					nuevoRestricted=true;
 				end
+			else
+				if nuevoBoost == true then
+					local inventory=player:getInventory();
+					local boost=getBoost(prof,inventory)
+					nuevoBoost = false;
+					player:getXp():AddXP(skill,level* (boost/100));
+				else
+					nuevoBoost=true;
+				end
 			end
 		end
 	end
 end
 
 Events.AddXP.Add(restrictXP)
+
